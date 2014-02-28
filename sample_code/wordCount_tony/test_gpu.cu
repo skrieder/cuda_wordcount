7234d3dd4a827f7c92c1e014956be04e602ad41b
mierl <leo.entropy@gmail.com>
2014-02-28 03:13:13 -0600

    debugging

--- sample_code/wordCount_tony/test_gpu.cu
+++ sample_code/wordCount_tony/test_gpu.cu
@@ -24,6 +24,13 @@ struct Hashtable{
 	//Lock lock[count];
 };
 
+unsigned int lenStr(unsigned char *str){
+	unsigned char *p=str;
+	while(*p!='\0')
+		p++;
+	return(p-str);
+}
+
 __device__ __host__ unsigned long hash_sdbm(unsigned char *str, unsigned long mod){
         unsigned long hash = 0; int c=0;
         while (c = *str++)
@@ -33,18 +40,21 @@ __device__ __host__ unsigned long hash_sdbm(unsigned char *str, unsigned long mo
         return hash % mod;
 }
 
-__device__ void put(unsigned char* key, Bucket* table, unsigned long mod){
+__device__ __host__ void put(unsigned char* key, Bucket* table, unsigned long mod){
 	unsigned long index = hash_sdbm(key, mod);
 	
-	table[index].lock.lock();
-	
-	//table[index]->key = key;
-	memcpy(table[index].key, key, sizeof(*key));
+//	table[index].lock.lock();
+	printf("put: pre-count =%ln\n", table[index].count); 
+	printf("put: pre-key=%s\n", table[index].key);
+	unsigned int l = lenStr(key);
+	printf("key len=%d\n",l);
+	memcpy(table[index].key, key, l+1 );
+	printf("get key=%s\n", table[index].key);
 	table[index].count ++;
+	printf("put: post-count =%ln\n", table[index].count);
+	printf("put: post-key=%s\n", table[index].key);
+//	table[index].lock.unlock();
 	
-	table[index].lock.unlock();
-	
-	//printf()
 
 }
 
@@ -52,6 +62,12 @@ __host__ __device__ void initTable(unsigned long size, Hashtable* i_table){
 	i_table = (Hashtable*)malloc(sizeof(Hashtable));	
 	i_table->count = size;
 	i_table->table = (Bucket*)malloc(size * sizeof(Bucket));
+	for(unsigned long i=0; i <= size; i++){
+		memset(i_table->table[i].key, '\0', 11 * sizeof(char));
+		i_table->table[i].count = 0;
+		
+		printf("init: blank count = %lu\n", i_table->table[i].count);
+	}
 }
 
 void copy_table_to_host(const Hashtable &devTable, Hashtable &hostTable) {	
@@ -74,7 +90,7 @@ __global__ void putTest(void){
 	printf("Now in the putTest on device. \n");
 	Hashtable * i_table;
 	initTable(4, i_table);
-        unsigned char* s1 = (unsigned char*) "abababab9";
+        unsigned char* s1 = (unsigned char*) "abab5";
         unsigned char* s2 = (unsigned char*) "abababab10";
         unsigned char* s3 = (unsigned char*) "cdababab9";
         unsigned char* s4 = (unsigned char*) "cdababab10";	
@@ -88,23 +104,38 @@ __global__ void kernel( void ) {
 	
 }
 
-
 int main ()
 {
-
-		
-	
 	unsigned long hash_size = 1024*1024;
 	unsigned long hashValue =0;
   	unsigned char str[] ="This, a sample string.";
 	unsigned long mod = 4;
-	char* s1 = "abababab9";
-	char* s2 = "abababab10";
-	char* s3 = "cdababab9";
-        char* s4 = "cdababab10";
+        unsigned char* s1 = (unsigned char*) "abab567";
+        unsigned char* s2 = (unsigned char*) "abababab10";
+        unsigned char* s3 = (unsigned char*) "cdababab9";
+        unsigned char* s4 = (unsigned char*) "cdababa8";	
 	printf("Before putTest\n");
-	putTest <<<1,1>>> ();
+//	putTest <<<1,1>>> ();
 	printf("After putTest\n");
+	Hashtable i_table;
+	
+	
+
+
+
+	initTable(4, &i_table); //problem on this reference
+	put(s1, (&i_table)->table, 4);
+//	put(s2, i_table->table, 4);
+//	put(s1, i_table->table, 4);
+//	put(s4, i_table->table, 4);
+        unsigned long index = hash_sdbm(s1, 4);
+      //  printf("original key = %s, find key = %s, count = %lu\n", s1, i_table->table[index].key, i_table->table[index].count);
+index = hash_sdbm(s2, 4);
+      //  printf("original key = %s, find key = %s, count = %lu\n", s2, i_table->table[index].key, i_table->table[index].count);
+index = hash_sdbm(s3, 4);
+      //  printf("original key = %s, find key = %s, count = %lu\n", s3, i_table->table[index].key, i_table->table[index].count);
+index = hash_sdbm(s4, 4);
+      //  printf("original key = %s, find key = %s, count = %lu\n", s4, i_table->table[index].key, i_table->table[index].count);
 
 
 
