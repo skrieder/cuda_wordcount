424b644c9bad0eb214e4327e0583faebaea67972
mierl <leo.entropy@gmail.com>
2014-03-04 16:19:37 -0600

    CPU and GPU tokenizer tested

--- sample_code/wordCount_tony/wordcount_helper.h
+++ sample_code/wordCount_tony/wordcount_helper.h
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int fileRead(FILE *fd, size_t size, void* buff);
 char *mystrtok(char* string,const char *delim);
+unsigned int lenStr(unsigned char *str);
+unsigned int tokenizeToArrayCPU(unsigned char* src,unsigned char** strArray);
+unsigned int tokenizeToArrayGPU(unsigned char* src,unsigned char** strArray);
+char *mystrtok(char* string,const char *delim);
+
